d08b7c9573fd7cfae357de1edbc919d9ea6f9374
LeeSungmin <lsmin01@gmail.com>
2020-02-03 20:48:34 +0900

    test pipeline deploy

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Elastic Beanstalk으로 배포된 AWS exercise의 A project입니다.');
+app.get("/", (req, res) => {
+    res.send("Elastic Beanstalk... Lee Sungmin Test...");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+    console.log(`Example app listening on port ${port}!`);
 });
 
-app.get('/health', (req, res) => {
-  res.status(200).send();
+app.get("/health", (req, res) => {
+    res.status(200).send();
 });
